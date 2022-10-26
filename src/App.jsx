b991ed19569ce1d7f63b560e56b7604a47c1196d
Raja Harsh Vardhan Singh <singhrajaharshvardhan004@gmail.com>
2022-10-27 01:40:14 +0530

    Created complete layout, added task layouts and separate stylesheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import './style.css'
 
 function App() {
-  return (<div> Hello </div>)
+  
+  return (
+    <>
+      <div className="main">
+        <div className='task-box'>
+          <div className='tasks'>
+            <div className='task-undo-modal'>
+              <button type='button'>Undo</button>
+            </div>
+
+            <div className='task-box-child'>
+              <div>
+                <div>
+                  <h3 >Title </h3>
+                  <div className="task-date">25/10/2022</div>
+                </div>
+                <div>
+                </div>
+              </div>
+              <div className='delete-box'>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
